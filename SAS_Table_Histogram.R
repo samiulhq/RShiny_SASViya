30443cd878b9e5883c7642b8ee9e5a3d0690b2ee
samiulhq <samiul.haque@live.com>
2023-11-27 14:33:12 -0500

    Update SAS_Table_Histogram.R

--- SAS_Table_Histogram.R
+++ SAS_Table_Histogram.R
@@ -1,14 +1,18 @@
 library(shiny)
 library(stringr)
-library(shinymanager)
-
-
+library(ggplot2)
+library(swat)
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
+
   titlePanel("SAS Tables Histogram Viewer"),
-  
+  tags$img(
+    src = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/SAS_logo_horiz.svg/2560px-SAS_logo_horiz.svg.png",
+    width = "100px",
+    height = "50px"
+  ),
   selectInput("caslibname", "Select a CASLIB", choices = c("Loading CASLIB List")),
   selectInput("tablename", "Select a Table", choices = c("Select CASLILB first")),
   selectInput("colname", "Select a Column", choices = c("Select a table first")),
@@ -43,11 +47,11 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram ----
 server <- function(input, output,session) {
  
-  library(swat)
+
   
   
   Sys.setenv(CAS_CLIENT_SSL_CA_LIST='/opt/anaconda3/my_CAS_Viya4_cert.pem')   
-  cassess<-CAS("example.sas.com",5570,username="username",password="password")
+  cassess<-CAS("mipistcas.eastus.cloudapp.azure.com",5570,username="sahaqu",password="sahaqu")
   
   
   
@@ -58,8 +62,15 @@ server <- function(input, output,session) {
   {
     currentCaslib <- req(input$caslibname)
     list_tables<-cas.table.tableInfo(cassess,caslib=currentCaslib)
-    updateSelectInput(session = session, inputId = "tablename", choices = req(list_tables$TableInfo$Name[str_order(list_tables$TableInfo$Name)]))
-    
+    if(length(list_tables)==0)
+    {
+      updateSelectInput(session = session, inputId = "tablename", choices ="No table in this library")
+      updateSelectInput(session = session, inputId = "colname", choices = "")
+    }
+    else
+    {
+    updateSelectInput(session = session, inputId = "tablename", choices = list_tables$TableInfo$Name[str_order(list_tables$TableInfo$Name)])
+    }
   }
   )
   
@@ -84,16 +95,26 @@ server <- function(input, output,session) {
     }
     
     
-  print(input$colname)
-  if(input$colname!="Select a table first"){  
+  #print(input$colname)
+  if(input$colname!="Select a table first" & input$tablename!="No table in this library" ){  
   output$distPlot <- renderPlot({
   
-    x    <- df[,input$colname]
+    #x    <- df[,input$colname]
+    #x <-as.data.frame(x)
+    x    <- to.r.data.frame(cas_table[,input$colname])
     bins <- seq(min(x, na.rm = TRUE), max(x,na.rm = TRUE), length.out = input$bins + 1)
     print(x)
-    hist(x, breaks = unique(bins), col = "#75AADB", border = "white",
-         xlab = input$colname,
-         main = paste("Histogram",input$tablename,".",input$colname), ylab="")
+    #hist(x, breaks = unique(bins), col = "#75AADB", border = "white",
+     #    xlab = input$colname,
+      #   main = paste("Histogram",input$tablename,".",input$colname), ylab="")
+    
+    ggplot(x, aes(.data[[input$colname]]),na.rm = TRUE) +
+      geom_histogram(bins = input$bins,
+                     fill = "steelblue3",
+                     colour = "grey30") +
+      xlab(input$colname) +
+      theme_minimal()
+    
     
   })}
   
